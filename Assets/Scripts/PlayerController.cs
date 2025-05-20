f15524d010fefa94d93d12f1cd08bec2ae47aed1
Apoorv0301-dev <asinghal2_be24@thapar.edu>
2025-05-20 15:43:50 +0530

    Character Movement
    Added Character movement, finalized 3D isometric gameplay

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    //movement variables
+    private Vector2 input;
+    private CharacterController controller;
+    private Vector3 dir;
+    [SerializeField] private float speed = 5f;
+
+    //rotation variables
+    private float currentVelocity;
+
+    //gravity and jumping
+    private float gravity = -9.81f;
+    [SerializeField] private float gravMultiplier = 3.0f;
+    [SerializeField] private float jumpPower;
+    private float velocity;
+
+    void Awake()
+    {
+        //Assigning player character controller to controller object
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        if (!isGrounded()) return;
+
+        velocity += jumpPower;
+    }
+    private void ApplyGravity()
+    {
+        if (isGrounded() && velocity < 0.0f)
+        {
+            velocity = -1.0f;
+        }
+        else
+        {
+            velocity += gravity * gravMultiplier * Time.deltaTime;
+        }
+        dir.y = velocity;
+    }
+    private void ApplyRotation()
+    {
+        //Preventing the player from automatically snapping to the forward direction while moving
+        if (input.sqrMagnitude == 0) return;
+
+        //Setting player rotation
+        float target = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target, ref currentVelocity, 0.05f);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+    }
+    private void ApplyMovement()
+    {
+        //Moving the Player
+        controller.Move(speed * Time.deltaTime * dir);
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        //Taking player input and accordinly setting the direction in which player needs to move
+        input = context.ReadValue<Vector2>();
+        dir = new Vector3(input.x, 0.0f, input.y);
+    }
+
+    private bool isGrounded() => controller.isGrounded;
+
+}
